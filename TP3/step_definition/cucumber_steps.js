4f03d694e82f9449139f8adb354111bd8b57880b
Jordan Quinveros <jordan@digeek.com>
2023-12-13 10:50:23 +0100

    ajout du TP3

---
+++ TP3/step_definition/cucumber_steps.js
@@ -0,0 +1,14 @@
+
+const { Given, When, Then } = require('cucumber');
+
+Given('I have {int} cucumbers in my basket', (cucumbers) => {
+  // Implementation code
+});
+
+When('I eat {int} cucumbers', (cucumbers) => {
+  // Implementation code
+});
+
+Then('I should have {int} cucumbers left', (remainingCucumbers) => {
+  // Implementation code
+});
